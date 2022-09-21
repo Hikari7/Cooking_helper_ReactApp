d99e466b1d3af0a745d8d80e04f26f9906946a49
Hikari7 <h.kobe712@gmail.com>
2022-09-21 10:23:15 -0700

    rendered info

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,32 +1,53 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import edamam from "../apis/edamam";
+import edamam from "../apis/api";
 
 const Search = () => {
-  const [term, setTerm] = useState("");
+  // const [term, setTerm] = useState("");
+  const [results, setResult] = useState([]);
+
+  // console.log(results);
 
   useEffect(() => {
-    const fetchRecipe = async () => {
-      const recipes = await axios.get(
+    const fetchDatas = async () => {
+      const datas = await axios.get(
         `https://api.edamam.com/search?q=chicken&app_id=${edamam.id}&app_key=${edamam.key}&from=0&to=3&calories=591-722`
       );
-      console.log(recipes.data.hits);
+
+      // console.log(datas.data.hits);
+      setResult(datas.data.hits);
     };
-    fetchRecipe();
+    fetchDatas();
   }, []);
+
+  // useEffect(() => console.log(results), [results]);
+
+  const renderedResults = results.map((result) => {
+    console.log(result);
+    return (
+      <div className="content">
+        <div className="item">{result.recipe.calories}</div>
+        <div className="item">
+          <img src={result.recipe.image}></img>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
       <form>
-        <label>Enter your favorite food! </label>
-        <input
-          value={term}
-          onChange={(e) => setTerm(e.target.value)}
-          className="input"
-        ></input>
+        <label>This is a recommended recipe! </label>
       </form>
+      <div>{renderedResults}</div>
     </div>
   );
 };
 
 export default Search;
+
+/* <input
+  value={term}
+  onChange={(e) => setTerm(e.target.value)}
+  className="input"
+/> */
