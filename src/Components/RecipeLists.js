cf690fde16abd19bbb34b0b65c3e5e71812c0da0
Hikari7 <h.kobe712@gmail.com>
2022-09-22 16:40:20 -0700

    added if

--- src/Components/RecipeLists.js
+++ src/Components/RecipeLists.js
@@ -7,31 +7,50 @@ const RecipeLists = () => {
 
   useEffect(() => {
     const fetchDatas = async () => {
-      const datas = await axios.get(
-        // `https://api.spoonacular.com/mealplanner/generate?apiKey=${api.key}`
-        // `https://api.spoonacular.com/recipes/random?apiKey=${api.key}`
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${api.key}`
-      );
-
-      console.log(datas.data.results);
-      setResult(datas.data.results);
+      if(results.length === 0){
+        const datas = await axios.get(
+  
+          // `https://api.spoonacular.com/mealplanner/generate?apiKey=${api.key}`
+          // `https://api.spoonacular.com/recipes/random?apiKey=${api.key}`
+          `https://api.spoonacular.com/recipes/complexSearch?apiKey=${api.key}`,
+          {
+            params: {
+              query: "pasta",
+            },
+          }
+        );
+        // console.log(datas.data.results);
+        //setItem = set the data (key, value )
+        //Convert a JavaScript object into a string
+        // localStorage.setItem("datas", JSON.stringify(datas.data));
+        setResult(datas.data.results);
+      }
+      
     };
     fetchDatas();
   }, []);
 
+  // useEffect(() => {
+  //   //save the datas which I fetched
+  //   const dataInLocalStorage = localStorage.getItem("datas");
+  //   if (dataInLocalStorage) {
+  //     console.log("No API call");
+  //     //JSON.parse = constructing the JavaScript value or object described by the string.
+  //     setResult(JSON.parse(dataInLocalStorage));
+  //   }
+  // }, []);
 
   const renderedResults = results.map((result) => {
     return (
-      <div className="content">
-          <div className="item">{result.title}</div>
-          <div className="item">
+      <div className="content" key={result.id}>
+        <div className="item">{result.title}</div>
+        <div className="item">
           <img src={result.image}></img>
         </div>
       </div>
     );
   });
-  
-  
+
   return (
     <div>
       <form>
